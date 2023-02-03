eb55cc02597009162434aacc99246716eefb162d
Zeratul4587 <margaritovkl@gmail.com>
2023-02-03 17:31:51 +0000

    first commit

---
+++ 01 Task/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишете израз, който да проверява дали дадено цяло число е четно
+//или нечетно.
+namespace _01_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
