d26b20f41f5f1b3207749ddd3e151d113d012d30
Zeratul4587 <margaritovkl@gmail.com>
2023-02-19 12:13:27 +0000

    your message

--- 01 Task/Program.cs
+++ 01 Task/Program.cs
@@ -1,12 +1,25 @@
 ﻿//Напишете израз, който да проверява дали дадено цяло число е четно
 //или нечетно.
+using System.Threading.Channels;
+
 namespace _01_Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number, remainder;
+            Console.WriteLine("Please enter an integer value: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            remainder = number % 2;
+            if (remainder ==0)
+            {
+                Console.WriteLine("{0} is an even number", number );
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd number", number);
+            }    
         }
     }
 }
---
+++ 03 Task/Program.cs
@@ -0,0 +1,22 @@
+﻿// Kiril Lazarov current employee file
+namespace _03_Task
+{
+    using System;
+    using System.Threading;
+    using System.Globalization;
+    using System.Data;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2023, 05, 2, 20, 30, 22);
+            int age = 34;
+            int salary = 2000;
+            Console.WriteLine($"The current date is {d}.My name is Kiril and i am {age} years old");
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("gb-GB");
+            Console.WriteLine("My current salary is {0:c}", 2000);
+            Console.Read();
+        }
+    }
+}
